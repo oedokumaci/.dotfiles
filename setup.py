f7a403bcbd882549f0489fd9c825ce1911b626b0
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-04-18 13:11:35 +0300

    refactor: use pathlib Path not os.path.expanduser

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import subprocess
-import os
+from pathlib import Path
 
 DOT_FILES = [".zshrc", ".gitconfig"]
 
@@ -8,15 +8,20 @@ if __name__ == "__main__":
         # install homebrew
         subprocess.run(["/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"], check=False)
 
+    if (input("Install oh-my-zsh? (Y/n): ") or "y") == "y":
+        # install oh-my-zsh
+        subprocess.run(["sh", "-c", "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"], check=False)
+
+    if (input("Install the software in the brewfile? (Y/n): ") or "y") == "y":
         # install software in the brewfile
-        subprocess.run(["brew", "bundle", "--file", "~/.dotfiles/Brewfile"], check=False)
+        subprocess.run(["brew", "bundle", "--file", str(Path.home() / ".dotfiles" / "Brewfile")], check=False)
 
     for dot_file in DOT_FILES:
-        if (input(f"Install {dot_file}? (Y/n): ") or "y") == "y":            
+        if (input(f"Install {dot_file}? (Y/n): ") or "y") == "y":
             # move the dotfile to .dotfiles
-            src_file = os.path.expanduser(f"~/{dot_file}")
-            dst_file = os.path.expanduser(f"~/.dotfiles/{dot_file}")
-            subprocess.run(["mv", src_file, dst_file], check=False)
+            src_file: Path = Path.home() / dot_file
+            dst_file: Path = Path.home() / ".dotfiles" / dot_file
+            subprocess.run(["mv", str(src_file), str(dst_file)], check=False)
 
             # create symlink in the home directory to the real file in the repo
-            subprocess.run(["ln", "-s", dst_file, src_file], check=False)
+            subprocess.run(["ln", "-s", str(dst_file), str(src_file)], check=False)
