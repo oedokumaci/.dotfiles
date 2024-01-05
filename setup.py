bca64ba7b5303dd99b828acba34c0a14e4a933e0
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2024-01-05 13:24:56 +0300

    feat: add casefold and fix sym_dir not existing for .dotfiles

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from pathlib import Path
 DOT_FILES = [".aliases", ".env", ".gitconfig", ".tmux.conf", ".vimrc", ".zshrc"]
 
 if __name__ == "__main__":
-    install_homebrew = input("Install homebrew? (Y/n): " or "y")
+    install_homebrew = input("Install homebrew? (Y/n): ").casefold() or "y"
     if install_homebrew == "y":
         # install homebrew
         subprocess.run(["/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"], check=False)
 
     for dot_file in DOT_FILES:
-        if (input(f"Install {dot_file}? (Y/n): ") or "y") == "y":
+        if (input(f"Install {dot_file}? (Y/n): ").casefold() or "y") == "y":
             # move the dotfile to .dotfiles
             local_dot_file: Path = Path.home() / dot_file
             github_dot_file: Path = Path.home() / ".dotfiles" / dot_file
@@ -23,14 +23,17 @@ if __name__ == "__main__":
             # create symlink in the home directory to the real file in the repo
             subprocess.run(["ln", "-s", str(github_dot_file), str(local_dot_file)], check=False)
 
-    if (input("Install oh-my-zsh? (Y/n): ") or "y") == "y":
+    if (input("Install oh-my-zsh? (Y/n): ").casefold() or "y") == "y":
         # install oh-my-zsh
         subprocess.run(["/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"], check=False)
 
-    if install_homebrew and (input("Install the software in the brewfile? (Y/n): ") or "y") == "y":
+    if install_homebrew and (input("Install the software in the brewfile? (Y/n): ").casefold() or "y") == "y":
         # install software in the brewfile
         subprocess.run(["brew", "bundle", "--file", str(Path.home() / ".dotfiles" / "Brewfile")], check=False)
 
-    if (input("Your .dotfiles is kept at ~/.dotfiles. Do you want to create a symlink to it? (Y/n): ") or "y") == "y":
+    if (input("Your .dotfiles is kept at ~/.dotfiles. Do you want to create a symlink to it? (Y/n): ").casefold() or "y") == "y":
         sym_dir = input("Enter the absolute path to the directory you want to create the symlink in: ")
+        # Create dir if not exists
+        sym_dir = Path(sym_dir)
+        sym_dir.mkdir(parents=True, exist_ok=True)
         subprocess.run(["ln", "-s", str(Path.home() / ".dotfiles"), sym_dir], check=False)
